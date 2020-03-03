80d807449bd7b7d6f698f1a3e8be6837a3bfa4fe
Hemendra <hemendra1111@gmail.com>
2020-03-04 00:06:42 +0530

    checkin

--- S7/vision/imagetransforms.py
+++ S7/vision/imagetransforms.py
@@ -2,9 +2,11 @@ from torchvision import  transforms
 
 
 def trainTransform():
-    return transforms.Compose(
-                            [transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    return transforms.Compose([
+                                transforms.RandomCrop(32, padding=4),
+                                transforms.RandomHorizontalFlip(),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                             ])
 
 
